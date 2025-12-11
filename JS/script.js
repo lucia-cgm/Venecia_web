05a66176f7ebb7dca2a7a5d447dd72892ba5938b
lucia-cgm <luciacaballego04@gmail.com>
2025-12-11 11:38:14 +0100

    carrusel automatico JS

--- JS/script.js
+++ JS/script.js
@@ -49,36 +49,36 @@ $(document).ready(function () {
  */
 var indice = 1;
 
-
-/**
- * Presenta la imagen que corresponda a partir del índice
- */
 function setImagen() {
     "use strict";
     var i;
-    //Se buscan los hijos de #galeria (figure), se calcula cuántos son
-    //y se buscan los hijos de #circulos (div)
+    //Se buscan los hijos de #galeria (figure), se calcula cuántos son y se buscan los hijos de #circulos (div)
     var imagenes = document.getElementById("galeria").children;
     var num_imagenes = imagenes.length;
     var circulos = document.getElementById("circulos").children;
 
-    //Se recorren el array figure y el de círculos para ocultar todos
-    //los figure y quitarle los class a los círculos
+    //Se recorren el array figure y el de círculos para ocultar todos los figure y quitarle los class a los círculos
     for (i = 0; i < num_imagenes; i += 1) {
         imagenes[i].style.display = "none";
         circulos[i].className = "";
     }
 
-    //Se muestra el figure correspondiente y se le pone la clase active
-    //al círculo asociado a esa posición
+    //Se muestra el figure correspondiente y se le pone la clase active al círculo asociado a esa posición
     imagenes[indice - 1].style.display = "block";
     circulos[indice - 1].className = "active";
+
+    //Se incrementa el índice. Se comprueba si ya es la última imagen del carrusel, en cuyo caso se vuelve a la primera
+    if (indice === imagenes.length) {
+        indice = 1;
+    } else {
+        indice += 1;
+    }
+    //Se añade el temporización de 5s (5000ms)
+    setTimeout(setImagen, 5000);
 }
 
 /**
- * Función llamada desde el HTML
- * Se le pasa como parámetro el número de orden que ocupa
- * la imagen en la galería
+ * Función llamada desde el HTML. Se le pasa como parámetro el número de orden que ocupa la imagen en la galería
  */
 function setImagenActual(n) {
     "use strict";
@@ -87,8 +87,7 @@ function setImagenActual(n) {
 }
 
 /**
- * Cuando se detecta que se ha cargado la página,
- * se muestra la primera imagen
+ * Cuando se detecta que se ha cargado la página, se muestra la primera imagen
  */
 window.onload = function () {
     "use strict";
